1f0a14ab8dbeceb8b4167a92934aa69a2a02694c
Jorge Fergusson <jorgefergusson@gmail.com>
2023-12-02 23:37:52 -0500

    Added routes for patient and physician scheduling management

--- app/routes/project.route.js
+++ app/routes/project.route.js
@@ -1,20 +1,23 @@
 // var express = require("express");
 // const router = express.Router();
 
-module.exports = (app) =>{
+module.exports = (app) => {
+  //user route endpoints
+  const users = require("../controllers/users.controller");
+  app.get("/users", users.findAll);
+  app.put("/users/:id", users.editOne);
+  app.delete("/users/:id", users.deleteOne);
 
-    //user route endpoints
-    const users = require('../controllers/users.controller');
-    app.get('/users', users.findAll);
-    app.put('/user/:id', users.editOne);
-    app.delete('/users/:id', users.deleteOne);
+  //role route endpoints
+  const roles = require("../controllers/roles.controller");
+  app.get("/roles", roles.findAll);
 
-    //role route endpoints
-    const roles = require('../controllers/roles.controller');
-    app.get('/roles', roles.findAll);
-
-    const appointments = require('../controllers/appointments.controller');
-    const chats = require('../controllers/chats.controller');
-}
+  const appointments = require("../controllers/appointments.controller");
+  app.get("/appointments", appointments.findAll);
+  app.post("/appointments/load", appointments.PhysicianLoadSchedule);
+  app.put("/appointments/schedule/:id", appointments.PatientSchedule);
+  app.put("/appointments/cancel/:id", appointments.PatientCancel);
+  const chats = require("../controllers/chats.controller");
+};
 
 //module.exports = router;
